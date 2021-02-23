d56d1b2262fc894ea97a6c1c00cdc87f370f6de1
Technifyaf <Technifyaf@gmail.com>
2021-02-23 16:36:26 +0200

    slider

--- sections/home/banner.js
+++ sections/home/banner.js
@@ -1,47 +1,68 @@
 import React from 'react';
 import { Box, Container, Grid, Button, Input, Heading, Text } from 'theme-ui';
-import {
-	CarouselProvider,
-	Slider,
-	Slide,
-	ButtonBack,
-	ButtonNext,
-} from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import Image from '../../components/image';
 
 import bannerImg from '../../assets/slide.jpg';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
 
-
-
+const slides = [
+	
+		{
+			title: 'Who we are',
+			description:
+				'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			button: 'Read More',
+			image: bannerImg,
+			user: 'Daniel',
+			userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+		},
+		{
+			title: 'What we do',
+			description:
+				'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.',
+			button: 'Discover',
+			image: 'https://i.imgur.com/DCdBXcq.jpg',
+			user: 'Samantha',
+			userProfile: 'https://s7.postimg.cc/ujy8zz7vv/5_3x.png',
+		},
+		{
+			title: 'Our gift section',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
+			button: 'Buy now',
+			image: 'https://i.imgur.com/DvmN8Hx.jpg',
+			user: 'Michael',
+			userProfile: 'https://s7.postimg.cc/6exjimijv/3_3x.png',
+		},
+		
+		
+	
+];
 const Banner = () => {
 	return (
 		<Box sx={styles.banner} id='banner'>
-		
-		
-					<CarouselProvider
-						naturalSlideWidth={100}
-						naturalSlideHeight={50}
-						totalSlides={3}
+			
+			<Slider autoplay={1000}>
+				{slides.map((item, index) => (
+					<Box sx = {styles.container}
+						key={index}
+						style={{
+							background: `url('${item.image}') no-repeat center center`,
+						}}
 					>
-						<Slider>
-							
-							<Slide index={0}>
-								<Image src={bannerImg} alt='' />
-							</Slide>
-							<Slide index={1}>
-								<Image src={bannerImg} alt='' />
-							</Slide>
-							<Slide index={2}>
-								<Image src={bannerImg} alt='' />
-							</Slide>
-						</Slider>
-						
-					</CarouselProvider>
-			
-			
-		</Box>
+						<Box sx ={styles.slide}>
+							<h1>{item.title}</h1>
+							<p>{item.description}</p>
+							<Button>{item.button}</Button>
+						</Box>
+					</Box>
+				))}
+			</Slider>
+		
+			</Box>
+		
 	);
 };
 
@@ -53,15 +74,14 @@ const styles = {
 		pb: ['50px', null, null, null, '0', null, '0'],
 		backgroundColor: 'background',
 		overflow: 'hidden',
-	
-		//backgroundSize: ['100%', null, null, null, 'cover'],
+		backgroundSize: ['100%', null, null, null, 'cover'],
 	},
 	container: {
 		backgroundColor: 'green',
 		width: [null, null, null, null, null, null, '1390px'],
 	},
 	grid: {
-    backgroundColor : 'gold',
+		backgroundColor: 'gold',
 		display: 'grid',
 		gridTemplateColumns: ['1fr', null, null, '1fr 1fr'],
 		gridGap: '0',
@@ -143,4 +163,12 @@ const styles = {
 			flex: ['0 0 100%', null, null, null, '0 0 auto'],
 		},
 	},
+
+	slide:{
+		display :'flex',
+		flexDirection :'column',
+		alignItems : 'center',
+		marginTop : '100px'
+
+	}
 };
