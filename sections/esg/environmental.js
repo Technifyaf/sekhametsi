f8cde606a6e23dc09b69f685969ad5c22d3d02c7
Technifyaf <Technifyaf@gmail.com>
2021-04-09 21:00:39 +0200

    enviromental

--- sections/esg/environmental.js
+++ sections/esg/environmental.js
@@ -4,34 +4,37 @@ import { Link } from '../../components/link';
 import Image from '../../components/image';
 import BlockTitle from '../../components/block-title';
 
-
 import img1 from '../../assets/agric.jpg';
 
 const Story = () => {
 	return (
 		<Box as='section' sx={styles.wrapper}>
 			<Container>
-			<BlockTitle
-          title="If you want to fast go alone, if you want to far go together"
-          text="The story of sekahmetsi"
-        />
+				<BlockTitle
+					title='If you want to fast go alone, if you want to far go together'
+					text='The story of sekahmetsi'
+				/>
 				<Box sx={styles.row}>
 					<Flex sx={styles.col}>
-							<Box sx={styles.content}>
-              <Box sx ={styles.titleBox}>
+						<Box sx={styles.content}>
+							<Box sx={styles.titleBox}>
 								<Heading as='h3'>Environmental</Heading>
-                <hr  color ="gold"></hr>
+								<hr color='gold'></hr>
 								<Text as='p'>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-									dapibus felis sed urna ultrices ornare. Cras elementum risus
-									sed cursus suscipit. Proin sem velit, blandit at maximus at,
-									consectetur vel tellus. Vestibulum sed iaculis nulla, finibus
-									molestie lorem. Morbi egestas mattis dolor, id rhoncus nisl
-									vulputate sit amet. Sed ornare blandit odio commodo mattis. 
+									It is critical that we all safeguard the future of our
+									beautiful country by conducting our business and affairs in a
+									responsible and sustainable manner and this begins and ends
+									with the way we deal and interact with our environment. The
+									majority of Lesotho is untouched landscape that requires
+									protection and consideration in our endeavours to grow the
+									economy of Lesotho. Sekhametsi is therefore dedicated to
+									assisting and championing environmentally sustainable projects
+									throughout Lesotho. We have responsibility to ensure that
+									future generations are able to enjoy their birth rite just as
+									we and generations before us have.
 								</Text>
 							</Box>
-              </Box>
-	
+						</Box>
 					</Flex>
 					<Flex sx={styles.col}>
 						<Image src={img1} alt='' />
@@ -49,7 +52,7 @@ const styles = {
 		pt: ['70px', null, null, '80px', '120px', null, '130px'],
 		backgroundColor: 'bacground',
 	},
-  row: {
+	row: {
 		mixBlendMode: 'softlight',
 		backgroundColor: 'black',
 		display: 'flex',
@@ -60,14 +63,14 @@ const styles = {
 		backgroundColor: 'background',
 		flex: ['0 0 100%', null, null, '0 0 50%'],
 	},
-  content: {
+	content: {
 		width: '100%',
 		textAlign: ['left', null, 'justify', null, 'left'],
 		pt: [null, null, null, null, null, '0px'],
 		pb: [null, null, null, null, null, '60px', '140px'],
-    pr: [null, null, null, null, null, '60px', '140px'],
+		pr: [null, null, null, null, null, '60px', '140px'],
 	},
-  titleBox: {
+	titleBox: {
 		textAlign: ['center', null, null, 'left'],
 		h3: {
 			color: 'primary',
