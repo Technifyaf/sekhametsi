a00ebb8eb00eff59f0e9aa6b38f350d407536ff0
Technifyaf <Technifyaf@gmail.com>
2021-11-04 11:51:53 +0200

    expo pic

--- sections/home/banner.js
+++ sections/home/banner.js
@@ -23,9 +23,9 @@ const slides = [
 	{
 		title: 'Dubai Expo 2020',
 		description:
-			' ',
+			'Sekhametsi Consortium was part of the Lesotho private sector delegation in Dubai Expo 2020',
 		button: 'Download presentation',
-		image: 'https://res.cloudinary.com/did2sewpc/image/upload/v1635929006/Dubai%20Expo/A06I8651_rxv42o.jpg',
+		image: 'https://res.cloudinary.com/did2sewpc/image/upload/v1636019237/Sekhametsi/dubai_ysa7pu.jpg',
 		user: 'Daniel',
 		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
 		link: 'https://drive.google.com/file/d/1_XbrmnC6IxTv1S4_CVUJaaRtqZ4bJllG/view?usp=sharing',
